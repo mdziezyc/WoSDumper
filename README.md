884aaca2ae33d1d9176ccf8d284a87bc029fe819
Maciej Dzieżyc <maciej.dziezyc@gmail.com>
2018-03-18 14:42:05 +0100

    Better example and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WoSPythonClient
-Web of Science Web Services Python client
+Web of Science Web Services Python client based on Zeep library (python-zeep.org)
+
+This client was created to fill the void of other WoS Client (https://github.com/enricobacis/wos) which didn't supported writing to JSONs (https://stackoverflow.com/q/47540354/2622708).
--- example.py
+++ example.py
@@ -3,5 +3,7 @@ from wosclient.wosclient import WoSClient
 if __name__ == '__main__':
     # Change 'login' for your login and 'password' for your password
     with WoSClient('login', 'password') as client:
+        json_object = client.search('TS=(cadmium OR lead)')
+        print(json_object)
+        print(json_object['records'][0])
         print(client.search('TS=(cadmium OR lead)', True).content)
-        print(client.search('TS=(cadmium OR lead)', False))
